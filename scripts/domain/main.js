7e69ebb510b3224ebc83adb3489a3143156d94cf
Luka Savovic <luka.savovic199323@gmail.com>
2018-04-15 23:17:36 +0200

    small changes

--- scripts/domain/main.js
+++ scripts/domain/main.js
@@ -18,18 +18,3 @@ closeX.addEventListener('click', function(){
     }, 500);
 });
 
-var url = 'https://openweathermap.org/data/2.5/weather?appid=b6907d289e10d714a6e88b30761fae22&id=792680&units=metric';
-
-var request = new XMLHttpRequest();
-request.open('GET', url);
-request.responseType = 'json';
-request.onload = function() {
-  
-  document.querySelector('a').innerHTML = "Today in " + request.response.name +":" ;
-  
-  console.log(request.response)
-  console.log(request.response.main.temp)
-  
-};
-
-request.send();

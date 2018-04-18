417d75a827880c7ab8183dd5b8db7b5011d6caca
Luka Savovic <luka.savovic199323@gmail.com>
2018-04-18 02:02:53 +0200

    added JSON

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -13,14 +13,23 @@ function markoFunkcija(args) {
 
     return true;
 }
-"use strict";
+'use strict';
 
-var galleryImg = document.querySelectorAll('.galleryBoxes div');
-console.log(galleryImg);
+var URL = 'https://api.myjson.com/bins/hd8ej ';
 
-for (var i = 0; i < galleryImg.length; i++) {
-    galleryImg[i].style.backgroundImage = "url(../img/tattoo/img" + i + ".jpg";
-}
+var request = new XMLHttpRequest();
+request.open('GET', URL);
+request.responseType = 'json';
+request.onload = function () {
+    for (var i = 0; i < request.response.length; i++) {
+        var gallery = document.querySelector('.galleryBoxes');
+        var galleryImg = document.createElement("DIV");
+        galleryImg.className = 'col-lg-4 col-md-6 col-sm-12 galleryImg';
+        galleryImg.style.backgroundImage = "url(../img/tattoo/" + request.response[i];
+        gallery.appendChild(galleryImg);
+    }
+};
+request.send();
 'use strict';
 
 var navButton = document.querySelector('nav');
--- scripts/domain/gallery_loading.js
+++ scripts/domain/gallery_loading.js
@@ -1,6 +1,16 @@
-var galleryImg = document.querySelectorAll('.galleryBoxes div');
-console.log(galleryImg);
 
-for(var i=0; i<galleryImg.length; i++) {
-    galleryImg[i].style.backgroundImage = "url(../img/tattoo/img" + i + ".jpg";
-}
+var URL = 'https://api.myjson.com/bins/hd8ej ';
+
+    var request = new XMLHttpRequest();
+    request.open('GET', URL);
+    request.responseType = 'json';
+    request.onload = function(){
+        for (var i=0; i<request.response.length;i++){
+            var gallery = document.querySelector('.galleryBoxes');
+            var galleryImg = document.createElement("DIV");
+            galleryImg.className = 'col-lg-4 col-md-6 col-sm-12 galleryImg';
+            galleryImg.style.backgroundImage = "url(../img/tattoo/"+request.response[i];
+            gallery.appendChild(galleryImg);
+        }
+    }
+request.send();

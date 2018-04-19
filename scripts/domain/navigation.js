11b7954d8be98d8947124a672a46c5484882b91b
DajanaMiljanovic <dajana.m.miljanovic@gmail.com>
2018-04-19 16:36:58 +0200

    update

--- scripts/domain/navigation.js
+++ scripts/domain/navigation.js
@@ -1,9 +1,9 @@
 var navButton = document.querySelector('nav');
 var menu = document.getElementById('menu');
-
-
+var svg = document.getElementById('Layer_1');
 
 navButton.addEventListener('click',function(){
+    svg.style.fill = "white";
     menu.classList.toggle('fadeIn');
     menu.classList.toggle("hidden");
     menu.classList.remove('fadeOut');
